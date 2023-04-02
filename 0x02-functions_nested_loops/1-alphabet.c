4e705dc94638700ba1a5460caad914761e08138a
Akintoye Kafilat <akintoyekafilat@gmail.com>
2023-04-02 07:32:33 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z' ; la++)
+		_putchar(la);
+
+	_putchar('\n');
+}
